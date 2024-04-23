6d8f4ca876bfe6fd45a73149d42f3fda8ad70ee8
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2024-04-23 23:45:56 +0300

    Header,main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css';
+
 import styled from 'styled-components';
+import {Header} from "./layout/header/Header";
+import {Main} from "./layout/sections/main/Main";
 
 
 function App() {
     return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
+        <StyledApp className="App">
+            <Header/>
+            <Main/>
+        </StyledApp>
     );
 }
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+const StyledApp = styled.section`
+`
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import mainPhoto from '../../../assets/images/main_photo.jpg'
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align='center' justify='space-around'>
+                <div>
+                    <span>Hi There</span>
+                    <Name>Im a Svetlana Dyablo</Name>
+                    <MainTitle>A Web Developer</MainTitle>
+                </div>
+                <Photo src={mainPhoto} alt="Svetlana Dyablo photo"/>
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #4dbeb1;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+const MainTitle = styled.h1`
+    
+`
+const Name = styled.h2`
+
+`
