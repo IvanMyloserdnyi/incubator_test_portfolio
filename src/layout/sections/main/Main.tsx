08df1591d3c7b70f10f2cf06043ceb6652c123d5
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2024-05-13 00:29:49 +0300

    Main adaptive, mixin

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align='center' justify='space-between'>
+                <FlexWrapper align='center' justify='space-around' wrap='wrap'>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am a <span>Svetlana Dyablo</span></Name>
@@ -33,10 +34,17 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    margin-right: 20px;
+    
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
 `
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
     
     &::before {
         content: '';
@@ -48,22 +56,34 @@ const PhotoWrapper = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1;
+
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -17px;
+            left: 20px;
+        }
     }
 `
 const MainTitle = styled.h1`
-    font-size: 27px;
-    font-weight: 400;
+    ${font({
+        weight: 400,
+        fMax: 27,
+        fMin: 20})}
 `
 const Name = styled.h2`
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 50px;
-    font-weight: 700;
+    ${font({
+        family:"'Josefin Sans', sans-serif",
+        weight: 700,
+        fMax: 50,
+        fMin: 36})}
     letter-spacing: 0.05em;
     margin: 10px 0;
     
     span {
         position: relative;
         z-index: 0;
+        white-space: nowrap;
         
         &::before {
             content: '';
@@ -76,6 +96,10 @@ const Name = styled.h2`
             bottom: 0;
             z-index: -1;
         }
+    }
+    
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
     }
 `
 const SmallText = styled.h2`
