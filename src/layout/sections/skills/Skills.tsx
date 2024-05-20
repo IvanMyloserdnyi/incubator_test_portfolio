b427e55d0696581d6c5644025e900cefe2f1cefc
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2024-05-20 18:20:47 +0300

    refactor

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -4,6 +4,7 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
 import {Container} from "../../../components/Container";
 import {S} from './Skills_Styles';
+import {Fade} from "react-awesome-reveal";
 
 const skillsData = [
     {
@@ -11,34 +12,41 @@ const skillsData = [
         skillTitle: 'html5',
         skillText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
     },
-    {iconId: 'css',
+    {
+        iconId: 'css',
         skillTitle: 'css3',
         skillText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
     },
-    {iconId: 'react',
+    {
+        iconId: 'react',
         skillTitle: 'React',
         skillText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
     },
-    {iconId: 'typeScript',
+    {
+        iconId: 'typeScript',
         skillTitle: 'TypeScript',
         skillText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
     },
-    {iconId: 'styledComponents',
+    {
+        iconId: 'styledComponents',
         skillTitle: 'Styled Components',
         skillText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
     },
-    {iconId: 'figma',
+    {
+        iconId: 'figma',
         skillTitle: 'Web Design',
         skillText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'
     }
 ]
-export const Skills:React.FC = () => {
+export const Skills: React.FC = () => {
     return (
         <S.Skills id='skills'>
             <Container>
                 <SectionTitle>My Skills</SectionTitle>
                 <FlexWrapper wrap='wrap' justify='space-between'>
-                    {skillsData.map(d => <Skill key={d.iconId} data={d}/>)}
+                    <Fade cascade damping={0.2}>
+                        {skillsData.map(d => <Skill key={d.iconId} data={d}/>)}
+                    </Fade>
                 </FlexWrapper>
             </Container>
         </S.Skills>
